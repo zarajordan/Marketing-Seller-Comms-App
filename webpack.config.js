7cb9f18de20f7bce68d35e8c641d80a3e852322a
Zara Jordan <zarajordan@Zaras-MacBook-Air.local>
2026-08-09 18:30:22 +0100

    fix: add publicPath for GitHub Pages deployment subfolder

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'docs'),
     filename: 'bundle.js',
+    publicPath: '/Marketing-Seller-Comms-App/',
   },
   module: {
     rules: [
